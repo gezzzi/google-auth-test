7b04bf94951aca1e61afeb8e4c0842667d9a4ad3
gezzzi <146487115+gezzzi@users.noreply.github.com>
2025-12-05 15:17:34 +0900

    リライトルールの削除

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/auth/google/callback",
-        destination: "/api/auth/callback/google",
-      },
-    ];
-  },
   images: {
     remotePatterns: [
       {
